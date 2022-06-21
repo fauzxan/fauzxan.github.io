625e3f4a649a5c412d660df4a39f40fb1ddd7dbf
Fauzaan <fauzaan.mm@gmail.com>
2022-06-21 11:27:08 +0800

    Template Design
    Next step: Add Routes

--- fauzaan/src/App.js
+++ fauzaan/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
+import SideBar from "./components/SideBar";
+import PageContent from "./components/PageContent";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div>
+			<SideBar />
+      <PageContent />
+		</div>
+	);
 }
 
 export default App;
